099859832022ffca40c3d4fef6b9155d9986ce30
SH4PO <47250302+SH4PO@users.noreply.github.com>
2020-10-14 15:33:41 +0200

    Create variables.sh

---
+++ includes/variables.sh
@@ -0,0 +1,33 @@
+## Paramètres
+
+CSI="\033["
+CEND="${CSI}0m"
+CRED="${CSI}1;31m"
+CGREEN="${CSI}1;32m"
+CPURPLE="${CSI}1;35m"
+CCYAN="${CSI}1;36m"
+RED='\e[0;31m'
+GREEN='\033[0;32m'
+BLUEDARK='\033[0;34m'
+BLUE='\e[0;36m'
+YELLOW='\e[0;33m'
+BWHITE='\e[1;37m'
+NC='\033[0m'
+DATE=`date +%d/%m/%Y-%H:%M:%S`
+IPADDRESS=$(hostname -I | cut -d\  -f1)
+CURRENTDIR="$PWD"
+BASEDIR="/opt/seedbox-compose"
+CONFDIR="/opt/seedbox"
+SERVICESAVAILABLE="$BASEDIR/includes/config/services-available"
+WEBSERVERAVAILABLE="$BASEDIR/includes/config/webserver-available"
+MEDIAVAILABLE="$BASEDIR/includes/config/media-available"
+SERVICES="$BASEDIR/includes/config/services"
+SERVICESUSER="/opt/seedbox/services-"
+MEDIASUSER="/opt/seedbox/media-"
+PACKAGESFILE="$BASEDIR/includes/config/packages"
+
+export NEWT_COLORS='
+  window=,white
+  border=green,blue
+  textbox=black,white
+'
